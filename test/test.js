d18b4b0126759c4476fe6a329ebdef0c99f2487b
jiwang <jiwang@alibaba-inc.com>
2015-04-14 14:25:18 +0800

    add: each test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,18 @@
-describe('noop', function(){
+describe('isFunction', function(){
   it('should check plain object', function(){
-    J.isFunction({}).should.be.False;
+    expect(J.isFunction({})).to.not.be.ok();
   });
+});
+decribe('each', function() {
+  it('should iterate own keys of an object', function() {
+    var obj = {
+      a: 1,
+      b: 2
+    };
+    J.each(obj, function(value, key){
+      return obj[key] = value * 2;
+    });
+    expect(obj.a).to.be.equal(2);
+    expect(obj.b).to.be.equal(4);
+  })
 })
